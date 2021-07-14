6d25a4683eb40f7c3d7ec92a604ecbcae468c2cd
elinah <elinah@webia.mg>
2021-07-14 12:35:02 +0000

    First commit

---
+++ src/components/AddTask.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class AddTask extends React.Component {
+    
+    handleSubmit = (e) => {
+        e.preventDefault()
+        //ajouter une nouvelle tache
+        this.props.onAddTask(this.newTask.value)
+        //rediriger l'utilisateur vers une autre page
+        this.props.history.push('/')
+    }
+        
+    
+    render(){
+        return(
+            <section>
+                <h1 className="m-3">Nouvelle tâche</h1>
+                <div className="card mx-3">
+                    <form className="card-body" onSubmit={(e) => this.handleSubmit(e)}>
+                        <div className="form-group">
+                            <label form="taskName">Nom de la tâche</label>
+                            <input type="text" className="form-control" name="taskName" id="taskName" required ref={input => this.newTask = input} />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Créer</button>
+                    </form>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default AddTask
